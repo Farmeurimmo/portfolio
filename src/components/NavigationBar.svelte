bf8929b541d3a88e552c4fba1026de85b6b264c1
Farmeurimmo <contact@farmeurimmo.fr>
2023-11-27 21:01:00 +0100

    Fixed the navbar vertical padding + overflow of body

--- src/components/NavigationBar.svelte
+++ src/components/NavigationBar.svelte
@@ -14,7 +14,7 @@
     }
 </script>
 
-<nav class="flex items-center w-screen border-0 mb-10 p-2 pt-3 bg-blend-soft-light bg-gradient-to-t from-gray-950 to-blue-950">
+<nav class="flex items-center w-screen border-0 mb-10 p-2 py-3 bg-blend-soft-light bg-gradient-to-t from-gray-950 to-blue-950">
     <div class="flex flex-row items-center justify-evenly w-screen h-16" id="items">
         <a class="text-white hover:text-gray-300 flex flex-row items-center gap-3" href="https://farmeurimmo.fr">
             <Image className="rounded-full border-0 w-14" src="https://cdn.farmeurimmo.fr/img/logo.jpg"/>
