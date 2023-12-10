2e7f34eba5f5c0fa59e564a3097ab8447568ced2
Farmeurimmo <contact@farmeurimmo.fr>
2023-12-10 20:20:34 +0100

    Tried to fix webhook url

--- src/routes/api/contact/+server.ts
+++ src/routes/api/contact/+server.ts
@@ -1,10 +1,19 @@
-import {DISCORD_WEBHOOK_URL} from '$lib/Env';
+import { DISCORD_WEBHOOK_URL } from '$env/static/private';
 
 export async function POST(event) {
+    console.log("POST /api/contact")
     const data = await event.request.formData()
     const email = data.get('email')
     const message = data.get('message')
     const name = data.get('name')
+
+    console.log(email)
+
+    if (!email || !message || !name) {
+        return new Response("Missing required fields", { status: 400 })
+    }
+
+    console.log(DISCORD_WEBHOOK_URL)
 
     await fetch(DISCORD_WEBHOOK_URL, {
         method: 'POST',
