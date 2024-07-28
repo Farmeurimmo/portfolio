c71da1193aa7095fe3649c5f13316b48d4f03b37
Farmeurimmo <contact@farmeurimmo.fr>
2024-07-28 18:35:06 +0200

    Some progress on reapersanction redaction

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -3,6 +3,18 @@
 	import NavigationBar from '../../components/NavigationBar.svelte';
 	import Background from '../../components/Background.svelte';
 	import CommonFooter from '../../components/CommonFooter.svelte';
+	import Image from '../../components/Image.svelte';
+
+	const projects = [
+		{
+			id: 'reapersanction',
+			title: 'Reaper Sanction',
+			description: 'Reaper Sanction est un plugin minecraft qui inclus un système de sanction intégré et de guis pour ' +
+				'sanctionner les joueurs du serveur. Les guis sont configurables et les sanctions sont personnalisables.',
+			tags: ['Spigot', 'Java', 'Minecraft', 'Plugin'],
+			img: 'https://cdn.farmeurimmo.fr/img/projects/89580.png'
+		}
+	];
 </script>
 
 <svelte:head>
@@ -19,6 +31,22 @@
 <section class="items-center flex flex-col min-h-screen gap-3 border-top">
 	<h1 class="text-6xl mt-8">{$_('pages.projects.title')}</h1>
 	<p class="text-2xl">{$_('pages.projects.description')}</p>
+	<div class="p-6 justify-center items-start grid md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-10">
+		{#each projects as project}
+			<a href="/projects/{project.id}" class="flex flex-col p-5 bg-gray-900 rounded-2xl
+     transform transition duration-500 hover:scale-105 hover:bg-gray-800 min-h-full" title={project.title}>
+				<Image src={project.img} className="rounded-t-2xl mt-0 p-0 h-96 w-full" />
+				<div class="py-3">
+					<h2 class="text-3xl font-bold">{project.title}</h2>
+					<p class="text-gray-400 mt-5">{project.description}</p>
+				</div>
+				<div class="flex flex-row gap-2">
+					{#each project.tags as tag}
+						<span class="bg-blue-900 text-gray-200 p-2 rounded-md">{tag}</span>
+					{/each}
+				</div>
+			</a>
+		{/each}
 </section>
 </body>
 
---
+++ src/routes/projects/reapersanction/+page.svelte
@@ -0,0 +1,34 @@
+<script>
+	import { _ } from 'svelte-i18n';
+	import Background from '../../../components/Background.svelte';
+	import CommonFooter from '../../../components/CommonFooter.svelte';
+	import NavigationBar from '../../../components/NavigationBar.svelte';
+
+	let good_points = $_('pages.projects.reapersanction.good_points');
+
+</script>
+
+<svelte:head>
+	<title>{$_('pages.projects.reapersanction.title')}</title>
+	<link href="https://farmeurimmo.fr/projects/reapersanction" rel="canonical" />
+	<meta content="{$_('pages.projects.reapersanction.description')}" name="description" />
+</svelte:head>
+
+<Background />
+
+<NavigationBar />
+
+<body class="flex flex-col min-h-screen px-4">
+<section class="items-center flex flex-col min-h-screen gap-3 border-top">
+	<h1 class="text-6xl mt-8">{$_('pages.projects.reapersanction.title')}</h1>
+	<p class="text-2xl w-1/2">{$_('pages.projects.reapersanction.description')}</p>
+	<ul class="list-disc list-inside text-xl mt-8">
+		{#each good_points as point}
+			<li>{point}</li>
+		{/each}
+	</ul>
+	<p class="mt-10" />
+</section>
+</body>
+
+<CommonFooter />
