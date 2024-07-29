faf8d55092603c945ef49a937c33865513cbd4c7
Farmeurimmo <contact@farmeurimmo.fr>
2024-07-29 20:20:35 +0200

    Added some buttons to go to the top and go back

--- src/routes/projects/reapersanction/+page.svelte
+++ src/routes/projects/reapersanction/+page.svelte
@@ -7,6 +7,7 @@
 	import yaml from 'svelte-highlight/languages/yaml';
 	import ashes from 'svelte-highlight/styles/ashes';
 	import Highlight from 'svelte-highlight';
+	import { onMount } from 'svelte';
 
 	let good_points = $_('pages.projects.reapersanction.good_points');
 	let indev_things = $_('pages.projects.reapersanction.indev_things');
@@ -42,6 +43,14 @@
 	];
 
 	const demo_images = [];
+
+	onMount(() => {
+		window.addEventListener('click', (event) => {
+			if (event.target.id === 'scrollToTop') {
+				window.scrollTo({ top: 0, behavior: 'smooth' });
+			}
+		});
+	});
 </script>
 
 <svelte:head>
@@ -56,7 +65,17 @@
 <NavigationBar />
 
 <body class="flex flex-col items-center min-h-screen px-4">
-<section class="items-center flex flex-col min-h-screen gap-5 border-top">
+<button class="visible text-4xl font-extrabold fixed bottom-8 right-8 bg-gray-800 text-white p-2 rounded-full z-40"
+				id="scrollToTop">&uarr;
+</button>
+<a class="fixed bottom-24 right-8 p-2 bg-bl rounded-full bg-gray-800 text-white" href="/projects">
+	<svg class="h-8 w-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+		<path
+			d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1h2a1 1 0 001-1V10m-6 0v4"
+			stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></path>
+	</svg>
+</a>
+<section class="items-center flex flex-col min-h-screen gap-5 border-top" id="scrollToTop">
 	<div class=" w-5/6 lg:3/4">
 		<h1 class="text-6xl mt-8 font-bold">{$_('pages.projects.reapersanction.title')}</h1>
 		<p class="text-2xl">{$_('pages.projects.reapersanction.description')}</p>
